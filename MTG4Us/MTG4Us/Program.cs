90192ae5d6d8d9a21644bdf10fb149c5577bd894
Thiago Odasaki <thijapones@gmail.com>
2019-08-15 23:18:20 -0300

    Criadas as Business

--- MTG4Us/MTG4Us/Program.cs
+++ MTG4Us/MTG4Us/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace MTG4Us
+namespace Application
 {
     public class Program
     {
--- MTG4Us/MTG4Us/Startup.cs
+++ MTG4Us/MTG4Us/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MTG4Us
+namespace Application
 {
     public class Startup
     {
